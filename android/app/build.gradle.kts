2b851cf90edd2ab08de313e1c15de546c4712ff0
xaistrying <xaistrying@gmail.com>
2025-07-22 16:39:44 +0700

    feat: launcher icon setup and release app optimization

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -35,6 +35,12 @@ android {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
+
+            // Enables code-related app optimization.
+            isMinifyEnabled = true
+
+            // Enables resource shrinking.
+            isShrinkResources = true
         }
     }
 }
